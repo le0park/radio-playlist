dd95022246614b121859695da23eaedb1db8229d
Junyoung Park <carpe0308@naver.com>
2022-10-02 11:42:31 +0900

    feat: clean code

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const { YTMUSIC } = require('ytmusic')
 
 const API = new YTMUSIC(document.cookie)
 
+const playlistId = 'PLma835fHaaztKoG97FEpgMh_qp-bSuHmr';
+
 const musics = [
     { "title": "Welcome to The Internet", "artist": "Bo Burnham" },
     { "title": "lovememore.", "artist": "dosii" },
@@ -11,11 +13,33 @@ const musics = [
 
 // playlist 정보를 가져온다. 
 // 노래를 검색한다. 
-const searchPromises = musics.map(({title, artist}) => API.search(`${artist} ${title}`, { filter: 'songs' }).then((results) => results[0].id))
 
-// 노래 정보가 있으면
-    // 가져온 노래 정보를 playlist 에 추가한다. 
-Promise.allSettled(searchPromises).then((results) => console.log(results) || API.addToPlaylist(results.map(({ value }) => value), 'PLma835fHaaztKoG97FEpgMh_qp-bSuHmr'))
+console.log(`>> search musics`)
+const searchPromises = musics.map(
+    ({title, artist}) => 
+        API.search(`${artist} ${title}`, { filter: 'songs' })
+            .then((results) => results.length > 0 ? results[0].id : null)
+)
 
-// 노래 정보가 없으면 
-    // 실패 목록에 추가한다. 
+const failedResults = 
+Promise.allSettled(searchPromises)
+    .then((results) => {
+        results = results.map((result, index) => ({ ...result, target: musics[index] }))
+
+        // 노래 정보가 있으면
+            // 가져온 노래 정보를 playlist 에 추가한다. 
+        const fulfilled = results.filter(result => result.status === 'fulfilled' && result.value);
+
+        console.log(`>> add playlists: ${JSON.stringify(fulfilled.map(result => result.value))}`)
+        API.addToPlaylist(fulfilled.map(({ value }) => value), playlistId)
+
+        const rejected = results.filter(result => result.status === 'rejected' || !result.value);
+        
+        // 노래 정보가 없으면 
+            // 실패 목록에 추가한다. 
+        console.log(`>> show failed: ${JSON.stringify(rejected.map(result => result.target))}`)
+        console.log(JSON.stringify(rejected, null, 2));
+    });
+
+
+
