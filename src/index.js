e874e3ee46003076502b11860f5b7fd79d251fb8
Junyoung Park <carpe0308@naver.com>
2022-10-02 11:42:31 +0900

    fix: clean

--- src/index.js
+++ src/index.js
@@ -1,45 +1,35 @@
-const { YTMUSIC } = require('ytmusic')
+const { YTMUSIC } = require('ytmusic');
+const { parsePlaylist } = require('./parse');
+const { addToPlaylist } = require('./ytmusic');
 
 const API = new YTMUSIC(document.cookie)
 
+// 노래를 저장할 playlist ID
 const playlistId = 'PLma835fHaaztKoG97FEpgMh_qp-bSuHmr';
 
-const musics = [
-    { "title": "Welcome to The Internet", "artist": "Bo Burnham" },
-    { "title": "lovememore.", "artist": "dosii" },
-    { "title": "Typa Girl", "artist": "블랙핑크" },
-    { "title": "사랑한다는 말", "artist": "김동률" }
-]
+const MusicController = {
+    musics: [],
+    fail: [],
+    success: [],
 
-// playlist 정보를 가져온다. 
-// 노래를 검색한다. 
+    parseMusics() {
+        return parsePlaylist();
+    },
 
-console.log(`>> search musics`)
-const searchPromises = musics.map(
-    ({title, artist}) => 
-        API.search(`${artist} ${title}`, { filter: 'songs' })
-            .then((results) => results.length > 0 ? results[0].id : null)
-)
-
-const failedResults = 
-Promise.allSettled(searchPromises)
-    .then((results) => {
+    async addMusicsToPlaylist(musics) {
+        // 노래를 검색한다. 
+        let results = await Promise.allSettled(search(musics))
         results = results.map((result, index) => ({ ...result, target: musics[index] }))
-
-        // 노래 정보가 있으면
-            // 가져온 노래 정보를 playlist 에 추가한다. 
-        const fulfilled = results.filter(result => result.status === 'fulfilled' && result.value);
-
-        console.log(`>> add playlists: ${JSON.stringify(fulfilled.map(result => result.value))}`)
-        API.addToPlaylist(fulfilled.map(({ value }) => value), playlistId)
-
-        const rejected = results.filter(result => result.status === 'rejected' || !result.value);
-        
-        // 노래 정보가 없으면 
-            // 실패 목록에 추가한다. 
+    
+        // 노래 정보가 있으면 playlist 에 추가한다.
+        this.success = results.filter(result => result.status === 'fulfilled' && result.value);
+        await addToPlaylist(this.success);
+    
+        // 노래 정보가 없으면 실패 목록에 추가한다. 
+        this.fail = results.filter(result => result.status === 'rejected' || !result.value);        
         console.log(`>> show failed: ${JSON.stringify(rejected.map(result => result.target))}`)
         console.log(JSON.stringify(rejected, null, 2));
-    });
+    }
+}
 
-
-
+console.log(MusicController.parseMusics());
