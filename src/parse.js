1a217aa8b7a79cd697d26e6167137ca761b779d9
Junyoung Park <carpe0308@naver.com>
2022-10-03 17:59:59 +0900

    fix: handle no music row

--- src/parse.js
+++ src/parse.js
@@ -1,8 +1,10 @@
 export function parsePlaylist(table) {
-    return Array.from(table.children).map(rowElement => {
-        return {
-            title: rowElement.querySelector('p.title').innerText,
-            artist: rowElement.querySelector('p.singer').innerText,
-        }
-    })
+    return Array.from(table.children)
+        .filter(element => element.querySelector('td').classList.length === 0)
+        .map(element => {
+            return {
+                title: element.querySelector('p.title').innerText,
+                artist: element.querySelector('p.singer').innerText,
+            }
+        })
 }
