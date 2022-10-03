4f87d2010d39c724dc496425439b15382e49e3e5
Junyoung Park <carpe0308@naver.com>
2022-10-03 18:43:35 +0900

    fix: clean code

--- src/content-script.js
+++ src/content-script.js
@@ -6,26 +6,37 @@ import { search, addToPlaylist, getPlaylist, getPlaylists} from "./ytmusic";
  * - Deal with requests to youtube or something because of CORS issues.
  */
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-    // If the received message has the expected format...
     if (msg.action === 'get_musics') {
+        /**
+         * Parse a music table and return the musics.
+         */
         const table = document.querySelector('#songlist_frame').contentDocument.querySelector('#musicView > table > tbody')
         sendResponse(parseMusicTable(table));
     } else if (msg.action === 'search_musics') {
+        /**
+         * Search the musics from youtube and return musics with id.
+         */
         const {musics} = msg.params;
         search(musics).then(values => 
             chrome.runtime.sendMessage('alaidjagmphdjliibkabmpcdflmagglj', { action: 'search_result', params: {values, musics} }));
     } else if (msg.action === 'get_playlists') {
+        /**
+         * Get playlist and return the playlist with id.
+         */
         getPlaylists().then(values => 
             chrome.runtime.sendMessage('alaidjagmphdjliibkabmpcdflmagglj', { action: 'get_playlists_result', params: {values} }));
     } else if (msg.action === 'add_musics_playlist') {
+        /**
+         * Add the musics to playlist and return the request status.
+         */
         const {musicIds, playlistId} = msg.params;
 
         let targetIds = [...musicIds];
         getPlaylist(playlistId)
             .then(({ content }) => {
                 const existIds = content.map(({ id }) => id);
-                console.log(JSON.stringify(existIds));
                 targetIds = targetIds.filter(id => !(id in existIds))
+
                 addToPlaylist(targetIds, playlistId)
                     .then(({status}) => {
                         chrome.runtime.sendMessage(
